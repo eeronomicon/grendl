2c512fce248b8879c3e4f10010387337ef03b889
HK Kahng, Jon Toler, Ryan Apking and Stephen Burden <hk@>
2016-10-06 13:48:40 -0700

    add head.html.

--- web/index.php
+++ web/index.php
@@ -2,15 +2,3 @@
     $website = require_once __DIR__.'/../app/app.php';
     $website->run();
 ?>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Michroma" rel="stylesheet">
-        <link rel="stylesheet" href="/css/styles.css" type="text/css">
-        <script src="/js/jquery-3.1.1.min.js" type="text/javascript"></script>
-        <script src="/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="/js/scripts.js" type="text/javascript"></script>
-        <title>Space Truckin</title>
-    </head>
-</html
---
+++ web/js/trade_scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $('#Ore_buy_input').bind('input', function() { 
+        $(this).val() // get the current value of the input field.
+    });
+
+});
